92dd6d640fb9300daf87c4df1d9c36700de1a0f4
benjamin OHANDJA <bendevtech16@gmail.com>
2023-10-11 15:59:56 +0100

    service d'enregistrement des micro services et demarrage de plusieurs instances  qui seron gerer par le microserviceProxy ou gateway
    mise en place du servire d'eregistrement des micro-services

--- company-service/src/main/java/cm/ben/companyservice/CompanyServiceApplication.java
+++ company-service/src/main/java/cm/ben/companyservice/CompanyServiceApplication.java
@@ -3,11 +3,13 @@ package cm.ben.companyservice;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import cm.ben.companyservice.dao.CompanyRepository;
 import cm.ben.companyservice.entities.Company;
 
+@EnableDiscoveryClient // on importe le client eurekaDiscovry
 @SpringBootApplication
 public class CompanyServiceApplication {
 
@@ -19,11 +21,14 @@ public class CompanyServiceApplication {
 	@Bean
 	CommandLineRunner start(CompanyRepository companyRepository) {
 		return args -> {
-
 			companyRepository.save(new Company(null, "cs consulting", 100 + Math.random() * 2000));
 			companyRepository.save(new Company(null, "bogenie sarl", 100 + Math.random() * 2000));
 			companyRepository.save(new Company(null, "futuriste sarl", 100 + Math.random() * 2000));
 			companyRepository.save(new Company(null, "batipro sarl", 100 + Math.random() * 2000));
+			System.out.println("\n");
+			companyRepository.findAll().forEach(System.out::println);
+			System.out.println("-------------------------------------");
+
 		};
 	}
 
