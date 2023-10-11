98ff92133ec698d2d2c45b8053644fce5957b063
benjamin OHANDJA <bendevtech16@gmail.com>
2023-10-11 09:03:30 +0100

    init
    init project

---
+++ company-service/src/test/java/cm/ben/companyservice/CompanyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cm.ben.companyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
